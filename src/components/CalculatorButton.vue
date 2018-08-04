0ea2e9937fabf42811e9924bedb88963cb75d06d
Gyf_linux <guo812773673@163.com>
2018-08-04 15:32:54 +0800

    初始化项目

---
+++ src/components/CalculatorButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="handleClick">
+    <span>{{ text }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'CalculatorButton',
+
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  },
+
+  methods: {
+    handleClick () {
+      this.$emit('click', this.text)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ test/unit/util.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+
+let id = 0
+const createElm = function () {
+  const elm = document.createElement('div')
+
+  elm.id = 'app' + ++id
+  document.body.appendChild(elm)
+
+  return elm
+}
+
+// 删除Vue实例
+exports.destoryVm = function (vm) {
+  vm.$destory && vm.$destory()
+  vm.$el && vm.$el.parentNode && vm.$el.parentNode.removeChild(vm.$el)
+}
+
+// 创建一个Vue实例
+exports.createVm = function (Comp, mounted = false) {
+  if (Object.prototype.toString.call(Comp) === '[Object String]') {
+    Comp = { template: Comp }
+  }
+
+  return new Vue(Comp).$mount(mounted !== false ? createElm() : null)
+}
+
+// 创建一个测试组件实例
+exports.createTest = function (Comp, propsData = {}, mounted = false) {
+  if (propsData === true || propsData === false) {
+    mounted = propsData
+    propsData = {}
+  }
+
+  const elm = createElm()
+  const Constructor = Vue.extend(Comp)
+  return new Constructor({ propsData }).$mount(mounted !== false ? elm : null)
+}
